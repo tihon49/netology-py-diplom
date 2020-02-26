a1f5763d1b48789465a28cb0ad31ce15c60a1dce
Tihon49 <tihon49@gmail.com>
2020-02-26 13:59:32 +0300

    main loop added. todo: choice 1

--- vk_api.py
+++ vk_api.py
@@ -1,25 +1,16 @@
 from config import *
 import requests
 from pprint import pprint
-import threading
+import time
 
 
 
-access_token = APP_TOKEN        # токен приложения
-app_id       = APP_ID           # id приложения
+access_token = APP_TOKEN    # токен приложения
+app_id       = APP_ID       # id приложения
 VERSION      = 5.103
 
 
-
-# Отдельный поток - обёртка для последующих функций
-def thread(my_func):
-    def wrapper(*args, **kwargs):
-        my_thread = threading.Thread (target=my_func, args=args, kwargs=kwargs)
-        my_thread.start()
-
-    return wrapper
-
-
+############################# ОПИСАНИЕ КЛАССА ###############################
 # Задача №1
 class User:
     def __init__(self, id):
@@ -50,18 +41,21 @@ class User:
     # Задача №2
     # битовое И (x & y)
     def __and__(self, user2):
-        user_1_friends = self.get_friends()
-        user_2_friends = user2.get_friends()
-        common_friends = []
+        try:
+            user_1_friends = self.get_friends()
+            user_2_friends = user2.get_friends()
+            common_friends = []
 
-        for friend in user_1_friends:
-            if friend in user_2_friends:
-                common_friends.append(User(friend))
-        
-        if common_friends:
-            return common_friends
-        else:
-            return 'у данных пользователей нет общих групп'
+            for friend in user_1_friends:
+                if friend in user_2_friends:
+                    common_friends.append(User(friend))
+            
+            if common_friends:
+                return common_friends
+            else:
+                return 'у данных пользователей нет общих друзей'
+        except:
+            pass
 
 
     # отправка request'a / получение response'а
@@ -71,59 +65,72 @@ class User:
         return data
 
 
-    # получаем список групп пользователя. type => list
-    def get_groups_names(self):
-        url = f'https://api.vk.com/method/groups.get' #'users.getSubscriptions'
+    # получаем друзей из группы
+    def get_group_contacts(self, g_id):
+        url = 'https://api.vk.com/method/groups.getMembers'
         params = {'access_token': access_token,
-                  'user_id': self.id,
-                  'extended': 1,
-                  'fields': 'members_count,counters,contacts,city,country',
-                  'count': 1000,
+                  'group_id': g_id,
+                  'filter': 'friends',
                   'v': VERSION
                   }
 
-        data = self.get_response(url, params)
-        groups = data['response']['items']
-        return groups # [group['name'] for group in groups]
+        data = self.get_response(url, params=params)
+        return data
+
+
+    # получаем список групп пользователя. type => list
+    def get_groups_names(self):
+        try:
+            url = f'https://api.vk.com/method/groups.get' #'users.getSubscriptions'
+            params = {'access_token': access_token,
+                      'user_id': self.id,
+                      'extended': 1,
+                      'fields': 'members_count,counters,contacts,city,country',
+                      'count': 1000,
+                      'v': VERSION
+                      }
+
+            data = self.get_response(url, params)
+            try:
+                groups = data['response']['items']
+            except:
+                pass
+            return groups
+        except:
+            print('не удалось найти пользователя с указанным id')
 
 
     # получаем список с друзьями. type => list
     def get_friends(self): 
-        url = f'https://api.vk.com/method/friends.get'
-        params = {'access_token': access_token,
-                  'v': VERSION,
-                  'user_id': self.id,
-                  'order': 'hints',
-                  'fields': 'nickname,domain,city,photo_200_orig,online',
-                  'name_case': 'nom'
-                  }
+        try:
+            url = f'https://api.vk.com/method/friends.get'
+            params = {'access_token': access_token,
+                      'v': VERSION,
+                      'user_id': self.id,
+                      'order': 'hints',
+                      'fields': 'nickname,domain,city,photo_200_orig,online',
+                      'name_case': 'nom'
+                      }
 
-        data = self.get_response(url, params)
-        friends_id = [friend['id'] for friend in data['response']['items']]
-        return friends_id
+            data = self.get_response(url, params)
+            friends_id = [friend['id'] for friend in data['response']['items']]
+            return friends_id
+        except:
+            return 'не удалось найти пользователя с указанным id'
 
 
     # получаем список групп в ктороых состоит только данный пользователь
-    def get_uniq_grous(self):
-        self.groups = self.get_groups_names()
-        counter = len(self.get_friends())
+    def get_uniq_groups(self, g_id):
+        url = 'https://api.vk.com/method/groups.getMembers'
+        params = {'access_token': access_token,
+                  'group_id': g_id,
+                  'filter': 'friends',
+                  'v': VERSION
+                  }
 
-        for friend in self.get_friends():
-            print(f'осталось обработать {counter} друзей')
-            counter -= 1
-            friend = User(friend)
-            
-            for group in self.groups:
-                try:
-                    if group in friend.get_groups_names():
-                        try:
-                            self.groups.remove(group)
-                        except:
-                            pass
-                except:
-                    pass
-
-        return self.groups
+        data = self.get_response(url, params=params)
+        count_of_friends = data['response']['count']
+        return count_of_friends
 
 
     # получаем список друзей онлайн. type => dict
@@ -140,14 +147,16 @@ class User:
 
 
     # найти друга с которым больше всего общих друзей
-    @thread
+    # @thread  # когда включены потоки, в терминале не отображается ход программы...
     def get_most_of_common_friends(self):
+        print('просьба набраться терпения, это может занять несколько минут.')
         max_count_of_friends = 0
         max_friends_id = 0
 
         friends_list = self.get_friends()
 
         for friend in friends_list:
+            time.sleep(1)
             try:
                 common_f_count = len(User(self.id) & User(friend))
                 friend_id = User(friend).id
@@ -161,45 +170,122 @@ class User:
                 print('.', end='')
 
         print(f'\nбольше всего общих друзей с пользователем: id{max_friends_id} - {max_count_of_friends}')
+################################### КОНЕЦ ОПИСАНИЯ КЛАССА ################################################
 
 
 # функция к домашнему заданию
 def homeWork():
     try:
         user_input = input('введите два id номера через пробел для поиска общих друзей: ').split()
-        user1 = User(int(user_input[0]))
-        user2 = User(int(user_input[1]))
+        if len(user_input) > 2:
+            print('введено больше двух id номеров')
+        else:
+            user1 = User(user_input[0])
+            user2 = User(user_input[1])
 
-        # вывод общих друзей в виде экземпляров класса с помощью метода  __and__(self, other)
-        pprint(user1 & user2)
+            # вывод общих друзей в виде экземпляров класса с помощью метода  __and__(self, other)
+            pprint(user1 & user2)
 
-        # выводим ссылку на профиль пользователя с помощью метода  __str__(self)
-        print(f'\nссылка на пользователя №1: {user1}')
-        print(f'ссылка на пользователя №2: {user2}')
+            # выводим ссылку на профиль пользователя с помощью метода  __str__(self)
+            print(f'\nссылка на пользователя №1: {user1}')
+            print(f'ссылка на пользователя №2: {user2}')
 
-    except Exception as e:
-        print(f'error: {e}')
-
-
-def main():
-    Evgeniy = User('eshmargunov')
-    Evgeniy_uniq_groups = Evgeniy.get_uniq_grous()
-    list_to_return = []
-
-    for group in Evgeniy_uniq_groups:
-        data = {'name': group['name'],
-                'gid' : group['id'],
-                'members_count': group['members_count']}
-        list_to_return.append(data)
-
-    with open('out.json', 'w') as f:
-        f.write(str(list_to_return))
-
-    print('Done')
+    except:
+        print('введены не верные данные')
 
 
 
-    
+# функция к дипломной работе
+def diplom_homework(user_name):
+    groups_list = user_name.get_groups_names()
+    uniq_groups = []
+
+    try:
+        for group in groups_list:
+            print('.', end='')
+            g_id = group['id']
+            time.sleep(1)
+            if user_name.get_uniq_groups(g_id) == 0:
+                data = {'name': group['name'],
+                        'gid' : group['id'],
+                        'members_count': group['members_count']
+                        }
+                uniq_groups.append(data)
+    except:
+        pass
+
+    if len(uniq_groups) == 0:
+        print('Done')
+        print('данный пользователь не состоит ни в одной "уникальной" группе')
+    else:
+        with open('groups.json', 'w') as f:
+            f.write(str(uniq_groups))
+
+        print('Done')
+        print(f'найдено {len(uniq_groups)} "уникальных" групп(ы)')
+        print('В папке со скриптом появился файл "groups.json"\nв котором записаны уникальные группы пользователя.')
+
+
+
+# функция получения экземпляра класса с указанным id
+def get_profile():
+    user_input = input('введите id полтзователя: ')
+    return User(user_input)
+
+
+
+def main(u_input, info):
+    try:
+        if u_input not in range(10):
+            print('введена не верная команда')
+        elif u_input == 0:
+            print(info)
+        elif u_input == 1:
+            homeWork()
+        elif u_input == 2:
+            print(get_profile())
+        elif u_input == 3:
+            pprint([group['name'] for group in get_profile().get_groups_names()])
+        elif u_input == 4:
+            pprint(get_profile().get_friends())
+        elif u_input == 5:
+            pprint(get_profile().get_friends_online())
+        elif u_input == 6:
+            diplom_homework(get_profile())
+        elif u_input == 7:
+            user = get_profile()
+            user.get_most_of_common_friends()
+        elif u_input == 8:
+            diplom_homework()
+    except ValueError:
+        print('просьба вводить только цифры')
+
+
 
 if __name__ == '__main__':
-    main()
+    info = '''
+вызвать данное меню еще раз ............................. : 0
+получить список общих друзей у двух пользователей (ДЗ)... : 1
+получить ссылку на профиль в вк ......................... : 2
+получить список названий всех групп пользователя ........ : 3
+получить список id всех друзей .......................... : 4
+получить список id друзей онлайн ........................ : 5
+получить список уникальных групп пользователя (ДИПЛОМ) .. : 6
+получить id пользователя с кем больше всего общих друзей  : 7
+#############################################################
+      [!]    ДЛЯ ВЫХОДА ИЗ ПРОГРАММЫ НАЖМИТЕ: q    [!]
+'''
+    print(info)
+
+    while True:
+        try:
+            user_input = input('\nваш выбор: ')
+            if user_input.lower() == 'q':
+                break
+            else:
+                try:
+                    main(int(user_input), info)
+                except ValueError:
+                    print('просьба вводить только цифры')
+        except:
+            pass
